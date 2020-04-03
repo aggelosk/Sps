12e724b46e5fbc5d6b47a891cd00fe2a3e3d1444
aggelosk <koropoulis.a@gmail.com>
2020-04-03 14:10:11 +0300

    Add files via upload

--- sps_run.R
+++ sps_run.R
@@ -2,7 +2,7 @@ rm(list=ls())
 library(foreach)
 library(doMC)
 n <- 10000
-registerDoMC(12)
+registerDoMC(20)
 
 system("make")
 
@@ -15,12 +15,12 @@ seed <- sample(1:100000000, n,  replace=FALSE)
 #seed <- read.table("seed_left.txt", header=FALSE)[,1]
 
 #write.table(seed, file="seed_left.txt", quote=F, row.names=F, col.names=F)
-cmds <- paste(curDir, "/sps -gens 2000 -rows 3 -cols 3 -cpcta 500 -smps 10  -strt 0 0 50 -smps 10 -maxs 1000 -psnd 0.005 -rcmb 0.02  -seed ", seed, sep="")
+cmds <- paste(curDir, "/sps -gens 5000 -rows 5 -cols 5 -cpcta 100 -smps 10 -strt 0 0 10 -smps 10 -maxs 999 -psnd 0.005 -rcmb 0.2 -seed ", seed, sep="")
 #write.table(x=cmds, file="cmd.txt", append=F, quote=F, row.names=F, col.names=F)
 
 foreach(i =  1:n)%dopar%{
     setwd(file.path(curDir, name))
-    dirName <- paste("spat_origin", i, sep="")
+    dirName <- paste("neut_rec", i, sep="")
     working_dir<-file.path(paste(curDir, "/", name, sep=""), dirName)
     dir.create(working_dir, showWarnings=FALSE)
 
